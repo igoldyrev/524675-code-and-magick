861d621ea875d2fa93c11e7900c846cb90f55bc6
Ilya Goldyrev <goldirev12@gmail.com>
2018-04-02 13:26:38 +0500

    Выполнена отрисовка гистограммы с именами игроков и колтчеством очков

--- js/stat.js
+++ js/stat.js
@@ -9,7 +9,7 @@ var CLOUD_Y = 10;
 
 //Переменные гистограммы
 var GAP = 10;
-var FONT_HEIGHT = 15;
+var FONT_HEIGHT = 10;
 var HEIGHT = 150;
 var BAR_WIDTH = 40;
 var BAR_HEIGHT = HEIGHT - FONT_HEIGHT - GAP;
@@ -28,7 +28,34 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.fill();
 };
 
-window.renderStatistics = function (ctx) {
+var getMaxElement = function(arr) {
+  var maxElement = arr[0];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
+};
+
+window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, 100, 10, '#fff');
+
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.baseline = 'hanging';
+  ctx.fillText('Ура вы победили!', CLOUD_X + CLOUD_CURVE * 2, CLOUD_Y + GAP * 2);
+  ctx.fillText('Список результатов:', CLOUD_X + CLOUD_CURVE * 2, CLOUD_Y + GAP * 4);
+
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < players.length; i++) {
+    ctx.fillStyle = '#000';
+    ctx.fillText(players[i], CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - GAP - FONT_HEIGHT);
+    ctx.fillText(Math.round(times[i]),CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - GAP * 3 - FONT_HEIGHT - GAP - BAR_HEIGHT * times[i] / maxTime);
+    ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - GAP * 2 - FONT_HEIGHT - GAP - BAR_HEIGHT * times[i] / maxTime, BAR_WIDTH, BAR_HEIGHT * times[i] / maxTime);
+  }
 };
