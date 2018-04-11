931a062883074857956fdde5f705cab14a6d1976
Ilya Goldyrev <goldirev12@gmail.com>
2018-04-11 18:00:09 +0500

    Добавлены обработчики событий для открытия окна по клику и с помощью клавиатуры

--- js/setup.js
+++ js/setup.js
@@ -1,11 +1,77 @@
 'use strict';
-document.querySelector('.setup').classList.remove('hidden');
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = document.querySelector('.setup-close');
+var setupFireballWrap = document.querySelector('.setup-fireball-wrap');
+var setupWizardEyes = document.querySelector('.wizard-eyes');
+var setupFireballInput = document.getElementsByName('fireball-color');
+
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
 document.querySelector('.setup-similar').classList.remove('hidden');
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_COLORS_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARDS_FAERBOLS_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
+var getRandom = function (min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+var getRandomNumberArray = function (array) {
+  return getRandom(0, array.length);
+};
+var randomIndexFaerballColor = getRandomNumberArray(WIZARDS_FAERBOLS_COLORS);
+var randomIndexEyesColor = getRandomNumberArray(WIZARD_COLORS_EYES);
+
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+var randomColorFaerball = function () {
+  setupFireballWrap.style.background = WIZARDS_FAERBOLS_COLORS[randomIndexFaerballColor];
+};
+
+console.log(setupFireballInput.value)
+
+var randomColorEyes = function () {
+  setupWizardEyes.style.fill = WIZARD_COLORS_EYES[randomIndexEyesColor];
+};
+
+setupFireballWrap.addEventListener('click', randomColorFaerball);
+setupWizardEyes.addEventListener('click', randomColorEyes);
+
+setupOpen.addEventListener('click', openPopup);
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+setupClose.addEventListener('click', closePopup);
 
 var wizards = [];
 
