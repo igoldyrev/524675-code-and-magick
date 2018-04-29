acba8ed97d3f0fdb52afd0beea4031d403555a44
Ilya Goldyrev <goldirev12@gmail.com>
2018-04-29 13:54:07 +0500

    Добавлена система баллов, позволяющая определить уровень похожести мага

--- js/setup.js
+++ js/setup.js
@@ -29,28 +29,34 @@
   var coatColor;
   var eyesColor;
 
+  var getRank = function (wizard) {
+    var rank = 0;
+    if (wizard.colorCoat = coatColor) {
+      rank += 2;
+    }
+    if (wizard.colorEyes = eyesColor) {
+      rank += 1;
+    }
+    return rank;
+  };
+
+  var namesComparator = function (left, right) {
+    if (left > right) {
+      return 1;
+    } else if (left < right) {
+      return -1;
+    } else {}
+    return 0
+  };
+
   var updateWizards = function () {
-    var sameCoatAndEyesWizards = wizards.filter(function (it) {
-      return it.colorCoat === coatColor && it.colorEyes === eyesColor;
-    });
-    var sameCoatWizards = wizards.filter(function (it) {
-      return it.colorCoat === coatColor;
-    });
-    var sameEyesWizards = wizards.filter(function (it) {
-      return it.colorEyes === eyesColor;
-    });
-
-    var filteredWizards = sameCoatAndEyesWizards;
-    filteredWizards = filteredWizards.concat(sameCoatWizards);
-    filteredWizards = filteredWizards.concat(sameEyesWizards);
-    filteredWizards = filteredWizards.concat(wizards);
-
-    var uniqueWizards = filteredWizards.filter(function (it, i) {
-      return filteredWizards.indexOf(it) === i;
-    });
-    window.render(uniqueWizards);
-
-
+    window.render(wizards.sort(function (left, right) {
+      var rankDiff = getRank(right) - getRank(left);
+      if (rankDiff === 0) {
+        rankDiff = namesComparator(left.name, right.name);
+      }
+      return rankDiff;
+    }));
   };
 
   var getRandomElement = function (array) {
