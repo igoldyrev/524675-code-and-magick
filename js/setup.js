138b72a5fffb7e238f550de37b0ff96211fda818
Ilya Goldyrev <goldirev12@gmail.com>
2018-04-30 14:07:51 +0500

    Поправлена ошибка

--- js/setup.js
+++ js/setup.js
@@ -7,10 +7,10 @@
 
   var getRank = function (wizard) {
     var rank = 0;
-    if (wizard.colorCoat = coatColor) {
+    if (wizard.colorCoat === coatColor) {
       rank += 2;
     }
-    if (wizard.colorEyes = eyesColor) {
+    if (wizard.colorEyes === eyesColor) {
       rank += 1;
     }
     return rank;
