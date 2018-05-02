6e6504c82cbd690621cd944fda435340d608a976
Ilya Goldyrev <goldirev12@gmail.com>
2018-05-02 21:49:45 +0500

    Изменение координат с помощью функции-конструктора

--- js/dialog.js
+++ js/dialog.js
@@ -4,27 +4,37 @@
   var dialogSetup = setup.querySelector('.setup-user-pic');
   var dragged = false;
 
+  var Coordinate = function (x, y) {
+    this.x = x;
+    this.y = y;
+  };
+
+  // Coordinate.prototype ={
+  //   shift: function (shiftX, shiftY) {
+  //     this.x = startCoords.x - shiftX;
+  //     this.y = startCoords.y - shiftY;
+  //   }
+  // };
+
+
   dialogSetup.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
 
-    var startCoords = {
-      x: evt.clientX,
-      y: evt.clientY
-    };
+    var startCoords = new Coordinate(evt.clientX, evt.clientY);
 
     var onMouseMove = function (moveEvt) {
       dragged = true;
       moveEvt.preventDefault();
+
+      // var shift = new Coordinate(moveEvt.clientX, moveEvt.clientY);
+      // shift.shift();
 
       var shift = {
         x: startCoords.x - moveEvt.clientX,
         y: startCoords.y - moveEvt.clientY
       };
 
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
+      startCoords = new Coordinate(moveEvt.clientX, moveEvt.clientY);
 
       var currentCoords = {
         x: setup.offsetLeft - shift.x,
